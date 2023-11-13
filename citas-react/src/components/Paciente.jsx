74500be0d2a648a365b0ad4c54df4c59d1c067be
Yander Avila <kixtersama@gmail.com>
2023-11-12 22:27:45 -0600

    Componentes Formulario y Pacientes

---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,27 @@
+function Paciente() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold m-3 text-gray-700 uppercase">Nombre: {''}
+        <span className="font-normal normal-case">Hook</span>
+        </p>
+        
+        <p className="font-bold m-3 text-gray-700 uppercase">Propietario: {''}
+        <span className="font-normal normal-case">Xavier</span>
+        </p>
+        
+        <p className="font-bold m-3 text-gray-700 uppercase">Email: {''}
+        <span className="font-normal normal-case">correo@correo.com</span>
+        </p>
+        
+        <p className="font-bold m-3 text-gray-700 uppercase">Fecha Alta: {''}
+        <span className="font-normal normal-case">12 Diciembre 2022</span>
+        </p>
+        
+        <p className="font-bold m-3 text-gray-700 uppercase">Síntomas: {''}
+        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, suscipit ducimus doloribus odio cumque, ex aspernatur maxime dolor eligendi fugit quae, unde non. Quo eius aut voluptate ullam doloribus perferendis.</span>
+        </p>
+    </div>
+  )
+}
+
+export default Paciente

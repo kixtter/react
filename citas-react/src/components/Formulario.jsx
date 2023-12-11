dff512271c2e1b56ddc3967de3452ceaaef565d1
Yander Avila <kixtersama@gmail.com>
2023-12-10 23:15:46 -0600

    Mensaje de error, listado de pacientes, mandar información desde app hasta paciente, fase inicial en edición de paciente

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,8 +1,55 @@
 import {useState, useEffect} from 'react';
+import Error from './Error';
 
-const Formulario = () => {
+const Formulario = ({pacientes, setPacientes}) => {
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+
+  const [error, setError] = useState(false);
+
+  const generarId = () => {
+    const fecha = Date.now().toString(36);
+    const random = Math.random().toString(36).substring(2);
+
+    return fecha + random;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validación del formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    //Objeto de paciente
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas,
+      id: generarId()
+    }
+
+    setPacientes([...pacientes, objetoPaciente]);
+
+    //Reiniciar el formulario
+    setNombre('');
+    setPropietario('');
+    setEmail('');
+    setFecha('');
+    setSintomas('');
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
         <h2 className="font-black text-3xl text-center">Seguimiento Paciente</h2>
 
         <p className="text-lg mt-5 text-center mb-10">
@@ -10,7 +57,11 @@ const Formulario = () => {
           <span className="text-indigo-600 font-bold">Administralos</span>
         </p>
 
-        <form className="bg-white shadow-md rounded-md py-10 px-5 mb-10">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded-md py-10 px-5 mb-10"
+        >
+          {error && <Error><p>Todos los campos son obligatorios</p></Error>}
           <div className="mb-5">
             <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
               Nombre Mascota
@@ -20,6 +71,8 @@ const Formulario = () => {
               type="text"
               placeholder="Nombre de la Mascota"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
             />
           </div>
 
@@ -32,6 +85,8 @@ const Formulario = () => {
               type="text"
               placeholder="Nombre del Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={propietario}
+              onChange={(e) => setPropietario(e.target.value)}
             />
           </div>
 
@@ -44,6 +99,8 @@ const Formulario = () => {
               type="email"
               placeholder="Email Contacto Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -55,6 +112,8 @@ const Formulario = () => {
               id="alta"
               type="date"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={fecha}
+              onChange={(e) => setFecha(e.target.value)}
             />
           </div>
 
@@ -66,6 +125,8 @@ const Formulario = () => {
               id="sintomas"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
               placeholder="Describe los síntomas"
+              value={sintomas}
+              onChange={(e) => setSintomas(e.target.value)}
             />
           </div>
 
--- citas-react/src/components/Paciente.jsx
+++ citas-react/src/components/Paciente.jsx
@@ -1,25 +1,40 @@
-function Paciente() {
+function Paciente({paciente, setPaciente}) {
+  const {nombre, propietario, email, fecha, sintomas} = paciente
+
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
         <p className="font-bold m-3 text-gray-700 uppercase">Nombre: {''}
-        <span className="font-normal normal-case">Hook</span>
+        <span className="font-normal normal-case">{nombre}</span>
         </p>
         
         <p className="font-bold m-3 text-gray-700 uppercase">Propietario: {''}
-        <span className="font-normal normal-case">Xavier</span>
+        <span className="font-normal normal-case">{propietario}</span>
         </p>
         
         <p className="font-bold m-3 text-gray-700 uppercase">Email: {''}
-        <span className="font-normal normal-case">correo@correo.com</span>
+        <span className="font-normal normal-case">{email}</span>
         </p>
         
         <p className="font-bold m-3 text-gray-700 uppercase">Fecha Alta: {''}
-        <span className="font-normal normal-case">12 Diciembre 2022</span>
+        <span className="font-normal normal-case">{fecha}</span>
         </p>
         
         <p className="font-bold m-3 text-gray-700 uppercase">Síntomas: {''}
-        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, suscipit ducimus doloribus odio cumque, ex aspernatur maxime dolor eligendi fugit quae, unde non. Quo eius aut voluptate ullam doloribus perferendis.</span>
+        <span className="font-normal normal-case">{sintomas}</span>
         </p>
+
+        <div className="flex justify-between mt-10">
+          <button
+            type="button"
+            className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+            // Usar arrow function cuando queramos pasar un argumento a una función
+            onClick={() => setPaciente(paciente)}
+          >Editar</button>
+          <button
+            type = "button"
+            className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+            >Eliminar</button>
+        </div>
     </div>
   )
 }
